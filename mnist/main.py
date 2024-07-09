4890170a1d7b8bd21a183dcaf5a6bbacc451da83
holmista <mishadaro21@gmail.com>
2024-07-10 02:25:18 +0400

    pass image path to main.py through argv

--- mnist/main.py
+++ mnist/main.py
@@ -1,4 +1,9 @@
 from nn import NN
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("image_path", help="Path to the image to predict")
+args = parser.parse_args()
 
 nn = NN.load_model("./model")
-print(nn.predict_from_image("mnist_png/3_0.png"))
+print(nn.predict_from_image(args.image_path))
